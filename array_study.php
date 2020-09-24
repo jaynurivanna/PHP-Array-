83d926d20e0be9a68c404744e5f0e3f27f695e3d
Jay Nurivanna <71619632+jaynurivanna@users.noreply.github.com>
2020-09-24 10:55:47 +0800

    Update array_study.php

--- array_study.php
+++ array_study.php
@@ -14,6 +14,13 @@ $color = array('white', 'green', 'red', 'blue', 'black');
 {
   echo $colors."<br />";
 }
+//Sorting Ascending
+sort($color);
+$length = count($color);
+for($x = 0; $x < $length; $x++) {
+  echo $color[$x];
+  echo "<br>";
+}
 
 //associate array
  $countries = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
@@ -21,6 +28,13 @@ $color = array('white', 'green', 'red', 'blue', 'black');
  foreach($countries as $country => $capital)
 {
   echo "The capital of $country is $capital"."<br />" ;
+}
+//display only the same capital and country name
+ foreach($countries as $country => $capital)
+{
+  if ($country == $capital) {
+     echo "The capital of $country is $capital"."<br />" ;
+   }
 }
 
 //Use count function 
@@ -47,6 +61,10 @@ foreach ($superheroes as $heros)
  {
   echo $heros['name'] . "\r\n";
   echo $heros['email'] . "<br />"; 
+}
+//display superhero name belongs with identification name
+foreach ($superheroes as $key => $hero) {
+  echo $hero['name'] ." is ".$key." email id ".$hero['email']. "<br />";
 }
  
 
